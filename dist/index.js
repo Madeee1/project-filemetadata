31f83a1671b3661fa9d6ce759576fd1401196d39
Madeee1 <madeee@duck.com>
2023-05-21 05:55:29 +0000

    feat: init commit 2

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,7 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import { config } from 'dotenv';
+config();
 var app = express();
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
